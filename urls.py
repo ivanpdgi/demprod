87e6bcec0748eec6727966bb25e79981239fc11c
ivanpdgi <ivanpdgi@gmail.com>
2016-12-26 17:25:20 +0300

    Add files via upload
    my version 1

--- urls.py
+++ urls.py
@@ -5,6 +5,10 @@ from . import views
 app_name = 'demprod'
 
 urlpatterns = [
+    url(r'^catalog/$', views.categoryes, name='categoryes'),
+    url(r'^add2basket/$', views.detail_basket, name='detail_basket'),
     url(r'^export_products/$', views.export_products, name='export_products'),
     url(r'^import_products/$', views.import_products, name='import_products'),
+    url(r'^export_categorys/$', views.export_categorys, name='export_categorys'),
+    url(r'^export_baskets/$', views.export_baskets, name='export_baskets'),
 ]
--- views.py
+++ views.py
@@ -5,12 +5,15 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
-from .models import Product, Basket, BasketItem
+from .models import Product, Category, Basket, BasketItem
 import csv
 import uuid
 import decimal
 import json
 from .forms import UploadFileForm
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 @login_required
@@ -100,6 +103,67 @@ def export_products(request):
             ])
     return response
 
+@login_required
+@staff_member_required
+def export_categorys(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="categorys.csv"'
+    writer = csv.writer(response)
+    writer.writerow([
+        'id',
+        'shortname',
+        'name',
+        'description',
+        'active',
+        'properties',
+        ])
+    for obj in Category.objects.all():
+        writer.writerow([
+            as_s(obj.id),
+            as_s(obj.shortname),
+            as_s(obj.name),
+            as_s(obj.description),
+            as_s(obj.active),
+            as_s(obj.properties)
+            ])
+    return response
+
+@login_required
+@staff_member_required
+def export_baskets(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="categorys.csv"'
+    writer = csv.writer(response)
+    writer.writerow([
+        'user id',
+        'user name',
+        'user phone',
+        'user email',
+        'created',
+        'posted',
+        'modified',
+        'state',
+        'shortname',
+        'cnt',
+        'price',
+        ])    
+    for basket in Basket.objects.filter(state=1):
+        for item in basket.basketitem_set.all():        
+         writer.writerow([
+            as_s(basket.user_id),
+            as_s(basket.user_name),
+            as_s(basket.user_phone),
+            as_s(basket.user_email),
+            as_s(basket.created),
+            as_s(basket.posted),
+            as_s(basket.modified),
+            as_s(basket.state),
+            as_s(item.shortname),
+            as_s(item.cnt),
+            as_s(item.price)
+            ])
+    return response
+
 def get_basket(request):
     bid = request.session.get('bid', None)
     if bid is None:
@@ -161,3 +225,30 @@ def del_from_basket(request):
     if basket_item.basket_ref.state == 0:
         basket_item.delete()
     return HttpResponse("Ok")
+
+def detail_basket (request):
+    return HttpResponse("Ok detail")
+
+
+def categoryes(request):
+    category_list = Category.objects.all()
+    cat_id = category_list[0].id
+    if 'cat_id' in request.GET:
+        cat_id = uuid.UUID(request.GET['cat_id'], version=4)
+    product_list = Product.objects.filter(categories=cat_id)
+    paginator = Paginator(product_list, 5) 
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products= paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    
+    return render (request, 'catalog.html', 
+                             {'category_list': category_list, 
+                              'cat_id': cat_id, 
+                              'products': products, 
+                              'paginator': paginator
+                              })
